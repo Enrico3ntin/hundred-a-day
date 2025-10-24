8b077db5235507eb5d99d9005dcd0e63fcd4f106
Enrico Trentin <enrico.trentin@gmail.com>
2025-10-24 09:36:55 +0100

    basic anonymous auth

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzOM1MjjhAQuIxrBWohNegx0cMk8q1Kjk",
+  authDomain: "hundredaday-3nsn.firebaseapp.com",
+  projectId: "hundredaday-3nsn",
+  storageBucket: "hundredaday-3nsn.firebasestorage.app",
+  messagingSenderId: "875622270682",
+  appId: "1:875622270682:web:6355e6a80c14cedb1ec7e3",
+  measurementId: "G-71FLRLHQJX"
+};
+
+export const app = initializeApp(firebaseConfig);
