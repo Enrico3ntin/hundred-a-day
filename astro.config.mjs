c17de15bba6a6e683145f68e6dc993a2056f8302
Enrico Trentin <enrico.trentin@gmail.com>
2025-10-26 14:27:24 +0000

    Remove unused server islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'middleware'
-  })
+  output: 'static'
 });
