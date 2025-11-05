37a03178c9bfb56e0b9da8338de90e6ca2a930e7
Enrico Trentin <enrico.trentin@gmail.com>
2025-11-05 16:03:34 +0000

    complete persistency

--- src/store/persistency.ts
+++ src/store/persistency.ts
@@ -1,6 +1,6 @@
-import { Database, getDatabase, ref, push, get, type DatabaseReference, onValue } from "firebase/database";
+import { Database, getDatabase, ref, push, type DatabaseReference, onValue, DataSnapshot, type Unsubscribe } from "firebase/database";
 import { $firebaseApp } from "./firebase";
-import { atom, onMount } from "nanostores";
+import { atom, onMount, computed, onSet, effect } from "nanostores";
 import { $uid } from "./authentication";
 
 const DATABASE_URL = "https://hundredaday-3nsn-default-rtdb.europe-west1.firebasedatabase.app";
@@ -10,30 +10,38 @@ onMount($database, () => {
     $database.set(getDatabase($firebaseApp.get(), DATABASE_URL));
 });
 
-const getDatabaseReference : (timestamp:Date, level?:'day'|'month') => Promise<DatabaseReference> =
-    async (timestamp, level="day") => {
-        const db = $database.get();
-        if (!db) throw Error("Database not found");
+const $currentTimeRef = atom<{ timestamp:Date, level?:'day'|'month'}>
+    ({ timestamp:new Date(), level:'day'});
+const $databaseRef = computed([$uid, $database, $currentTimeRef], (uid, db, { timestamp, level }) => {
+    if (!uid || !db) return;
+    const yyyymmdd = timestamp.toISOString().substring(0,10);
+    const yyyy = yyyymmdd.substring(0,4);
+    const mm = yyyymmdd.substring(5,7);
+    const path = [uid, yyyy, mm, (level=='day' ? yyyymmdd : undefined)].join('/');
+    return ref(db, path);
+});
+const $unsubscribe = atom< Unsubscribe | undefined >();
+onSet($unsubscribe, x => {
+    let unsubscribe = $unsubscribe.get();
+    if (unsubscribe) unsubscribe();
+});
+const $read = atom< ((snapshot:DataSnapshot)=>void) | undefined >();
+effect([$databaseRef, $read], (databaseRef, read)=>{
+    if (databaseRef && read) {
+        var unsubscribe = onValue(databaseRef, read);
+        $unsubscribe.set(unsubscribe);
+    }
+});
 
-        const uid = await $uid.get();
-        if (!uid) throw Error("uid not found");
-
-        const yyyymmdd = timestamp.toISOString().substring(0,10);
-        const yyyy = yyyymmdd.substring(0,4);
-        const mm = yyyymmdd.substring(5,7);
-        const path = [uid, yyyy, mm, (level=='day' ? yyyymmdd : undefined)]
-            .join('/');
-
-        return ref(db, path);
-    };
-
-export const retrieveDay (timestamp:Date) => {timestamp:Date, repCount:number}[] =
-    async (timestamp) => {
-        return await get(await getDatabaseReference(timestamp));
-        
-    };
+export const read : (callback:(snapshot:DataSnapshot)=>void) => Unsubscribe | undefined =
+    (callback) => {
+        $read.set(callback);
+        return $unsubscribe.get();
+    }
 
 export const persist : (timestamp:Date, repCount:number)=>void =
-    async (timestamp, repCount) => push(
-        await getDatabaseReference(timestamp), 
-        { timestamp:timestamp.getTime(), repCount:repCount });
+    (timestamp, repCount) => {
+        const reference = $databaseRef.get();
+        const session = { timestamp:timestamp.getTime(), repCount:repCount };
+        if (reference) push(reference, session);
+    }
